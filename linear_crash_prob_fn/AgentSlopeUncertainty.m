02beee9348ca931f5e250737c44a999dce233ea5
Brendan Gould <bgould2@uccs.edu>
2023-05-24 14:36:18 -0600

    Convert plot scripts to functions

--- linear_crash_prob_fn/AgentSlopeUncertainty.m
+++ linear_crash_prob_fn/AgentSlopeUncertainty.m
@@ -1,46 +1,53 @@
-% Goal of this script: Assume crash prob is linear: p(x) = ax + b.
-% Agents think they know the value of a, call it a_* (Signaling designer
-% knows the true value). Signaling designer calculates optimal (accident
-% minimizing) value of beta for the true value of a.
-% Agents then make behavior decisions using their guessed value a_* and the
-% chosen value of beta. We plot the increase in accident probability caused
-% by the "bad guess" by the agents.
+function AgentSlopeUncertainty(yInt, trueSignalProbFn, falseSignalProbFn, V2VMass, crashCost, granularity)
+	% Goal of this script: Assume crash prob is linear: p(x) = ax + b.
+	% Agents think they know the value of a, call it a_* (Signaling designer
+	% knows the true value). Signaling designer calculates optimal (accident
+	% minimizing) value of beta for the true value of a.
+	% Agents then make behavior decisions using their guessed value a_* and the
+	% chosen value of beta. We plot the increase in accident probability caused
+	% by the "bad guess" by the agents.
+	arguments (Input)
+		yInt double{mustBeInRange(yInt, 0, 1, "exclude-upper")}
+		trueSignalProbFn(1, 1) function_handle
+		falseSignalProbFn(1, 1) function_handle
+		V2VMass double{mustBeInRange(V2VMass, 0, 1)}
+		crashCost double{mustBeGreaterThan(crashCost, 1)}
+		granularity(1, 1) uint32 {mustBePositive} = 100
+	end
 
-clear;
+	% Calculate optimal beta for each true value of a
+	slopes = linspace(0, 1-yInt, granularity);
+	trueParams = WorldParams(slopes, yInt, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
+	[optimalBeta, optimalCrashProb] = GetOptimalBeta(trueParams);
 
-b = 0.1;
-trueSignalProbFn = @(y) 0.8 .* y;
-falseSignalProbFn = @(y) 0.1 .* y;
-V2VMass = 0.9;
-crashCost = 3;
+	% Get agent behavior decisions for incorrect guesses of a
+	[assumedSlopeMat, betaMat] = meshgrid(slopes, optimalBeta); % Note: we swapped the order of the meshgrid from signaler uncertainty to keep assumption on horiz axis
+	assumedParams = WorldParams(assumedSlopeMat, yInt, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
+	inducedBehavior = GetEqBehavior(assumedParams, betaMat);
 
-granularity = 100;
+	% Calculate loss from agents assumming wrong slope
+	actualSlopeMat = assumedSlopeMat.';
+	trueParamsMat = WorldParams(actualSlopeMat, yInt, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
+	inducedCrashProb = GetCrashProb(trueParamsMat, inducedBehavior, betaMat);
+	loss = inducedCrashProb - repmat(squeeze(optimalCrashProb), granularity, 1).';
 
-% Calculate optimal beta for each true value of a
-slopes = linspace(0, 1-b, granularity);
-trueParams = WorldParams(slopes, b, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
-[optimalBeta, optimalCrashProb] = GetOptimalBeta(trueParams);
+	% Plot
+	figure
+	heatmap(loss);
+	title("Loss Caused by Agent Slope Uncertainty");
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
 
-% Get agent behavior decisions for incorrect guesses of a
-[assumedSlopeMat, betaMat] = meshgrid(slopes, optimalBeta); % Note: we swapped the order of the meshgrid from signaler uncertainty to keep assumption on horiz axis
-assumedParams = WorldParams(assumedSlopeMat, b, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
-inducedBehavior = GetEqBehavior(assumedParams, betaMat);
+	figure
+	heatmap(double(loss < 0));
+	title("Where Agent Slope Uncertainty is Beneficial");
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
+end
 
-% Calculate loss from agents assumming wrong slope
-actualSlopeMat = assumedSlopeMat.';
-trueParamsMat = WorldParams(actualSlopeMat, b, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
-inducedCrashProb = GetCrashProb(trueParamsMat, inducedBehavior, betaMat);
-loss = inducedCrashProb - repmat(squeeze(optimalCrashProb), 100, 1).';
-
-% Plot
-figure
-heatmap(loss);
-title("Loss Caused by Agent Slope Uncertainty");
-xlabel("Assumed slope");
-ylabel("Actual slope");
-
-figure
-heatmap(double(loss < 0));
-title("Where Agent Slope Uncertainty is Beneficial");
-xlabel("Assumed slope");
-ylabel("Actual slope");
+% Initial param set 
+% yInt = 0.1;
+% trueSignalProbFn = @(y) 0.8 .* y;
+% falseSignalProbFn = @(y) 0.1 .* y;
+% V2VMass = 0.9;
+% crashCost = 3;
--- linear_crash_prob_fn/SignalerSlopeUncertainty.m
+++ linear_crash_prob_fn/SignalerSlopeUncertainty.m
@@ -1,33 +1,42 @@
-% Goal of this script: Assume crash prob is linear: p(x) = ax + b.
-% Signaling designer thinks they know the value of a, call it a_*. We then
-% calculate the optimal (accident minimizing) value of beta assuming p(x) =
-% a_*x + b.
-% Then, we calculate the accident probabilities induced by this beta for
-% values of a != a_*, and plot the increase in accident probability caused
-% by the "bad guess" by the signaling designer.
-clear;
+function SignalerSlopeUncertainty(yInt, trueSignalProbFn, falseSignalProbFn, V2VMass, crashCost, granularity)
+	% Goal of this script: Assume crash prob is linear: p(x) = ax + b.
+	% Signaling designer thinks they know the value of a, call it a_*. We then
+	% calculate the optimal (accident minimizing) value of beta assuming p(x) =
+	% a_*x + b.
+	% Then, we calculate the accident probabilities induced by this beta for
+	% values of a != a_*, and plot the increase in accident probability caused
+	% by the "bad guess" by the signaling designer.
+	arguments (Input)
+		yInt double{mustBeInRange(yInt, 0, 1, "exclude-upper")}
+		trueSignalProbFn(1, 1) function_handle
+		falseSignalProbFn(1, 1) function_handle
+		V2VMass double{mustBeInRange(V2VMass, 0, 1)}
+		crashCost double{mustBeGreaterThan(crashCost, 1)}
+		granularity(1, 1) uint32 {mustBePositive} = 100
+	end
 
-b = 0.1;
-trueSignalProbFn = @(y) 0.8 .* y;
-falseSignalProbFn = @(y) 0.1 .* y;
-V2VMass = 0.9;
-crashCost = 3;
+	% Calculate optimal beta for each assumed slope
+	slopes = linspace(0, 1-yInt, granularity);
+	assumedParams = WorldParams(slopes, yInt, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
+	[optimalBeta, optimalCrashProb] = GetOptimalBeta(assumedParams);
 
-granularity = 100;
+	% Calculate loss of assumed optimal beta on different slopes
+	[betaMat, slopeMat] = meshgrid(optimalBeta, slopes);
+	trueParams = WorldParams(slopeMat, yInt, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
+	inducedBehavior = GetEqBehavior(trueParams, betaMat);
+	inducedCrashProb = GetCrashProb(trueParams, inducedBehavior, betaMat);
+	loss = inducedCrashProb - repmat(squeeze(optimalCrashProb), granularity, 1).';
 
-% Calculate optimal beta for each assumed slope
-slopes = linspace(0, 1-b, granularity);
-assumedParams = WorldParams(slopes, b, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
-[optimalBeta, optimalCrashProb] = GetOptimalBeta(assumedParams);
+	% Plot
+    figure;
+	heatmap(loss);
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
+end
 
-% Calculate loss of assumed optimal beta on different slopes
-[betaMat, slopeMat] = meshgrid(optimalBeta, slopes);
-trueParams = WorldParams(slopeMat, b, V2VMass, crashCost, trueSignalProbFn, falseSignalProbFn);
-inducedBehavior = GetEqBehavior(trueParams, betaMat);
-inducedCrashProb = GetCrashProb(trueParams, inducedBehavior, betaMat);
-loss = inducedCrashProb - repmat(squeeze(optimalCrashProb), 100, 1).';
 
-% Plot
-fig = heatmap(loss);
-xlabel("Assumed slope");
-ylabel("Actual slope");
+% yInt = 0.1;
+% trueSignalProbFn = @(y) 0.8 .* y;
+% falseSignalProbFn = @(y) 0.1 .* y;
+% V2VMass = 0.9;
+% crashCost = 3;
