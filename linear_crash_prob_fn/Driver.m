f2cdb9a7d0ffb64519ce1e28d2d5aadf97cf5161
Brendan Gould <bgould2@uccs.edu>
2023-05-25 12:12:17 -0600

    Hook up sliders to agent uncertainty plots

--- linear_crash_prob_fn/Driver.m
+++ linear_crash_prob_fn/Driver.m
@@ -3,48 +3,14 @@ trueSignalProbFn = @(y) 0.8 .* y;
 falseSignalProbFn = @(y) 0.1 .* y;
 wp = WorldParams(0, 0.1, 0.9, 3, @(y) 0.8.*y, @(y) 0.1.*y);
 
-%% Signaler Uncertainty
-[crashProbWCertainty, crashProbWUncertainty] = SignalerSlopeUncertainty(wp);
-loss = crashProbWUncertainty - crashProbWCertainty;
-ShowUI(loss, trueSignalProbFn, falseSignalProbFn);
-
-% Plot
-% figure;
-% heatmap(loss);
-% title("Loss Caused by Signaler Slope Uncertainty")
-% xlabel("Assumed slope");
-% ylabel("Actual slope");
-
-%% Agent Uncertainty
-% [crashProbWCertainty, crashProbWUncertainty] = AgentSlopeUncertainty(wp);
-% loss = crashProbWUncertainty - crashProbWCertainty;
-%
-% % Plot
-% figure
-% heatmap(loss);
-% title("Loss Caused by Agent Slope Uncertainty");
-% xlabel("Assumed slope");
-% ylabel("Actual slope");
-%
-% figure
-% heatmap(double(loss < 0));
-% title("Where Agent Slope Uncertainty is Beneficial");
-% xlabel("Assumed slope");
-% ylabel("Actual slope");
+ShowUI(wp, trueSignalProbFn, falseSignalProbFn);
 
 %% Helper Functions
-function loss = UpdateLoss(hMap, crashProbEvalFn, worldParams)
-	[crashProbWCertainty, crashProbWUncertainty] = crashProbEvalFn(worldParams);
-	loss = crashProbWUncertainty - crashProbWCertainty;
-	hMap.ColorData = loss;
-	drawnow;
-end
-
-function ShowUI(loss, trueSignalProbFn, falseSignalProbFn)
+function ShowUI(worldParams, trueSignalProbFn, falseSignalProbFn)
 	uifig = uifigure();
-	g = uigridlayout(uifig, [3, 3]);
-	g.ColumnWidth = {'1x', '2x', '3x'};
-	g.RowHeight = {50};
+	g = uigridlayout(uifig, [3, 2]);
+	g.ColumnWidth = {'1x', '2x'};
+	g.RowHeight = {50, 50, 50};
 
 	% Sliders and labels
 	yIntLbl = uilabel(g, "Text", "y-intercept");
@@ -53,7 +19,7 @@ function ShowUI(loss, trueSignalProbFn, falseSignalProbFn)
 	yIntSlider = uislider(g, "Limits", [0, 1]);
 	yIntSlider.Layout.Row = 1;
 	yIntSlider.Layout.Column = 2;
-	yIntSlider.Value = 0.1; % TODO: get rid of these magic nums
+	yIntSlider.Value = worldParams.yInt;
 
 	V2VMassLbl = uilabel(g, "Text", "V2V Mass");
 	V2VMassLbl.Layout.Row = 2;
@@ -61,7 +27,7 @@ function ShowUI(loss, trueSignalProbFn, falseSignalProbFn)
 	V2VMassSlider = uislider(g, "Limits", [0, 1]);
 	V2VMassSlider.Layout.Row = 2;
 	V2VMassSlider.Layout.Column = 2;
-	V2VMassSlider.Value = 0.9; % TODO: get rid of these magic nums
+	V2VMassSlider.Value = worldParams.V2VMass;
 
 	crashCostLbl = uilabel(g, "Text", "Crash Cost");
 	crashCostLbl.Layout.Row = 3;
@@ -69,19 +35,60 @@ function ShowUI(loss, trueSignalProbFn, falseSignalProbFn)
 	crashCostSlider = uislider(g, "Limits", [1, 100]);
 	crashCostSlider.Layout.Row = 3;
 	crashCostSlider.Layout.Column = 2;
-	crashCostSlider.Value = 3; % TODO: get rid of these magic nums
+	crashCostSlider.Value = worldParams.crashCost;
 
-	% Heatmap
-	heatmapPanel = uipanel(g);
-	heatmapPanel.Layout.Row = [1, 3];
-	heatmapPanel.Layout.Column = 3;
-	h = heatmap(heatmapPanel, loss, "GridVisible", false);
+	% Heatmaps
+	sh = signalerHeatmap(worldParams);
+	[ahl, ahb] = agentHeatmaps(worldParams);
 
 	% Slider function hooks
-	yIntSlider.ValueChangingFcn = @(src, event) UpdateLoss(h, @SignalerSlopeUncertainty, ...
+	yIntSlider.ValueChangingFcn = @(src, event) UpdateLosses(sh, ahl, ahb, ...
 		WorldParams(0, event.Value, V2VMassSlider.Value, crashCostSlider.Value, trueSignalProbFn, falseSignalProbFn));
-	V2VMassSlider.ValueChangingFcn = @(src, event) UpdateLoss(h, @SignalerSlopeUncertainty, ...
+	V2VMassSlider.ValueChangingFcn = @(src, event) UpdateLosses(sh, ahl, ahb, ...
 		WorldParams(0, yIntSlider.Value, event.Value, crashCostSlider.Value, trueSignalProbFn, falseSignalProbFn));
-	crashCostSlider.ValueChangingFcn = @(src, event) UpdateLoss(h, @SignalerSlopeUncertainty, ...
+	crashCostSlider.ValueChangingFcn = @(src, event) UpdateLosses(sh, ahl, ahb, ...
 		WorldParams(0, yIntSlider.Value, V2VMassSlider.Value, event.Value, trueSignalProbFn, falseSignalProbFn));
+end
+
+function sh = signalerHeatmap(worldParams)
+	[crashProbWCertainty, crashProbWUncertainty] = SignalerSlopeUncertainty(worldParams);
+	loss = crashProbWUncertainty - crashProbWCertainty;
+
+	signalerFig = figure();
+	sh = heatmap(signalerFig, loss, "GridVisible", false);
+	title("Loss Caused by Signaler Slope Uncertainty")
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
+end
+
+function [ahl, ahb] = agentHeatmaps(worldParams)
+	agentFig = figure();
+	agentFig.Position(3) = 1120;
+	[crashProbWCertainty, crashProbWUncertainty] = AgentSlopeUncertainty(worldParams);
+	loss = crashProbWUncertainty - crashProbWCertainty;
+
+	subplot(1, 2, 1);
+	ahl = heatmap(loss, "GridVisible", false);
+	title("Loss Caused by Agent Slope Uncertainty");
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
+
+	subplot(1, 2, 2);
+	ahb = heatmap(double(loss < 0), "GridVisible", false);
+	title("Where Agent Slope Uncertainty is Beneficial");
+	xlabel("Assumed slope");
+	ylabel("Actual slope");
+end
+
+function UpdateLosses(sh, ahl, ahb, worldParams)
+	[crashProbWCertainty, crashProbWUncertainty] = SignalerSlopeUncertainty(worldParams);
+	signalerLoss = crashProbWUncertainty - crashProbWCertainty;
+	sh.ColorData = signalerLoss;
+
+	[crashProbWCertainty, crashProbWUncertainty] = AgentSlopeUncertainty(worldParams);
+	agentLoss = crashProbWUncertainty - crashProbWCertainty;
+	ahl.ColorData = agentLoss;
+	ahb.ColorData = double(agentLoss < 0);
+
+	drawnow;
 end
