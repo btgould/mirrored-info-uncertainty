140163c6cbcfecfafa4e876317a464f020291d71
Brendan Gould <bgould2@uccs.edu>
2023-05-30 16:30:52 -0600

    Fix overwritten eqs bug

--- linear_crash_prob_fn/GetOptimalBeta.m
+++ linear_crash_prob_fn/GetOptimalBeta.m
@@ -9,11 +9,19 @@ function [beta, crashProb, eqs] = GetOptimalBeta(worldParams)
 	end
 
 	crashProbs = squeeze(zeros(cat(2, 2, size(worldParams.slope))));
+	eqList = crashProbs;
 	for beta = [0, 1]
-		[behavior, eqs] = GetEqBehavior(worldParams, beta);
+		[behavior, eqList(beta+1, :)] = GetEqBehavior(worldParams, beta); % TODO: eqs gets overwritten on second iteration
 		crashProbs(beta+1, :) = GetCrashProb(worldParams, behavior, beta);
 	end
 
 	[crashProb, beta] = min(crashProbs);
+
+	eqs = zeros(size(beta, 2), 1);
+	for i = 1:size(beta, 2)
+		eqs(i) = eqList(beta(i), i);
+	end
+
+
 	beta = beta - 1; % Adjusts for offset caused by matlab's one indexing
 end
