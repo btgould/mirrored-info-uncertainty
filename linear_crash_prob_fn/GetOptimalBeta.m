e42667024b6de405684a4666b94d52027810eb17
Brendan Gould <bgould2@uccs.edu>
2023-06-06 16:33:21 -0600

    arbitrary choice for signaler indifferent fuzzy min

--- linear_crash_prob_fn/GetOptimalBeta.m
+++ linear_crash_prob_fn/GetOptimalBeta.m
@@ -15,13 +15,29 @@ function [beta, crashProb, eqs] = GetOptimalBeta(worldParams)
 		crashProbs(beta+1, :) = GetCrashProb(worldParams, behavior, beta);
 	end
 
-	[crashProb, beta] = min(crashProbs);
+	% [crashProb, beta] = min(crashProbs);
+	[crashProb, beta] = fuzzyMin(crashProbs, 1e-5);
 
 	eqs = zeros(size(beta, 2), 1);
 	for i = 1:size(beta, 2)
 		eqs(i) = eqList(beta(i), i);
 	end
 
-
 	beta = beta - 1; % Adjusts for offset caused by matlab's one indexing
+end
+
+function [minimum, idx] = fuzzyMin(data, eps)
+	% Get minimum
+	minimum = min(data);
+
+	% Get data indices
+	d = diff(data);
+	idx = zeros(1, size(data, 1));
+	idx(d > eps) = 1;
+	idx(d < -eps) = 2;
+
+	% Assign worst case to indices near zero
+	idx(idx == 0) = 2; % TODO: I have no justification for if this is actually worst case
+	% It looks nice initially, but I don't think that it is. I have found
+	% regions where there are still gaps 
 end
