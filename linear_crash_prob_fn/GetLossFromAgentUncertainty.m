57b9055a9786c4c7d0cd0d9486fc3e1200d602e4
Brendan Gould <brendan.gould314@gmail.com>
2023-08-30 15:56:20 -0600

    Better code for agent uncertainty

--- linear_crash_prob_fn/GetLossFromAgentUncertainty.m
+++ linear_crash_prob_fn/GetLossFromAgentUncertainty.m
@@ -1,6 +1,6 @@
-function loss = GetLossFromAgentUncertainty(worldParams, granularity)
+function loss = GetLossFromAgentUncertainty(realWP, granularity)
 	arguments (Input)
-		worldParams(1, 1) WorldParams
+		realWP(1, 1) WorldParams = WorldParams(0.7, 0.1, 0.9, 3, @(y) 0.6.*y, @(y) 0.3.*y);
 		granularity(1, 1) uint16{mustBePositive} = 100
 	end
 	arguments (Output)
@@ -8,8 +8,8 @@ function loss = GetLossFromAgentUncertainty(worldParams, granularity)
 	end
 
 	% Calculate worst case slope for each slope and uncertainty radius
-	slopes = linspace(0, 1-worldParams.yInt, granularity);
-	uncertaintyRadii = linspace(0, 1-worldParams.yInt, granularity);
+	slopes = linspace(0, 1-realWP.yInt, granularity); % This is OK because I assume p(0) is common knowledge
+	uncertaintyRadii = linspace(0, 1-realWP.yInt, granularity);
 	[slopeMat, uncertaintyMat] = meshgrid(slopes, uncertaintyRadii);
 	worstCaseSlopes = slopeMat - uncertaintyMat;
 
@@ -17,13 +17,61 @@ function loss = GetLossFromAgentUncertainty(worldParams, granularity)
 	worstCaseSlopes(worstCaseSlopes < 0) = 0;
 
 	% Calculate accident probabilty induced by uncertainty
-	agentBelievedWP = worldParams.Copy().UpdateSlope(slopeMat);
-	worstCaseWP = worldParams.Copy().UpdateSlope(worstCaseSlopes);
-	chosenBeta = GetOptimalBeta(worstCaseWP);
-	realizedBehavior = GetEqBehavior(agentBelievedWP, chosenBeta);
-	realizedCP = GetCrashProb(worldParams, realizedBehavior, chosenBeta);
+	agentBelievedWP = realWP.Copy().UpdateSlope(slopeMat);
+	worstCaseWP = realWP.Copy().UpdateSlope(worstCaseSlopes);
+    
+    realizedCP = GetInducedCPGS(realWP, agentBelievedWP, worstCaseWP);
 
 	% Calculate loss
-	[~, crashProbUnderCertainty] = GetOptimalBeta(worldParams);
+    crashProbUnderCertainty = GetOptimalCPGS(realWP, agentBelievedWP);
 	loss = realizedCP - crashProbUnderCertainty;
+end
+
+function inducedCP = GetInducedCPGS(realWP, agentBelievedWP, worstCaseWP)
+	arguments (Input)
+		realWP WorldParams
+        agentBelievedWP WorldParams
+        worstCaseWP WorldParams
+	end
+	arguments (Output)
+		inducedCP double
+    end
+
+    betas = linspace(0, 1, 100);
+    minWorstCaseCP = zeros(size(agentBelievedWP.slope)) + 2;
+    inducedCP = zeros(size(agentBelievedWP.slope)) + 2;
+
+    for beta = betas
+        anticipatedBehavior = GetEqBehavior(worstCaseWP, beta);
+        worstCaseCP = GetCrashProb(realWP, anticipatedBehavior, beta);
+
+        newMin = worstCaseCP < minWorstCaseCP;
+        minWorstCaseCP(newMin) = worstCaseCP(newMin);
+
+        inducedBehavior = GetEqBehavior(agentBelievedWP, beta);
+        inducedCPCurr = GetCrashProb(realWP, inducedBehavior, beta);
+
+        inducedCP(newMin) = inducedCPCurr(newMin);
+    end
+end
+
+function minCrashProb = GetOptimalCPGS(realWP, agentBelievedWP)
+	arguments (Input)
+		realWP WorldParams
+        agentBelievedWP WorldParams
+	end
+	arguments (Output)
+		minCrashProb double
+    end
+
+    betas = linspace(0, 1, 100);
+    minCrashProb = zeros(size(agentBelievedWP.slope)) + 2;
+
+    for beta = betas
+        behavior = GetEqBehavior(agentBelievedWP, beta);
+        crashProbs = GetCrashProb(realWP, behavior, beta);
+
+        newMin = crashProbs < minCrashProb;
+        minCrashProb(newMin) = crashProbs(newMin);
+    end
 end
